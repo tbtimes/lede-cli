812744b4d540c5c4dbc8272e58894b6673ff285c
Eli Murray <emurray@tampabay.com>
2016-08-22 11:36:31 -0400

    added tweetlines filter to project template

--- templates/project/compilerConfig.js
+++ templates/project/compilerConfig.js
@@ -22,6 +22,31 @@ module.exports = {
           }
         },
         {
+          name: "tweetlines",
+          fn: function (txt, url) {
+            var regex = new RegExp('tweet\{.+\}', 'g');
+            var tweetline = txt.match(regex);
+            if (tweetline) {
+              var replacer = tweetline.map(function (line, ind) {
+                var title = line.slice(6, -1);
+                var uniq = slug(title);
+                if (title.length > 117) {
+                  title = title.slice(0, 114) + '...';
+                }
+                return `<span class="tweetline" id="tweetline${uniq}" onclick="gigya.services.socialize.plugins.reactions.instances[\'componentDiv${uniq}\'].buttonInstances[\'componentDiv${uniq}-reaction0\'].onClick()">${line.slice(
+                  6,
+                  -1)}<script type="text/javascript">var act = new gigya.socialize.UserAction();act.setTitle("${title}");act.setLinkBack("${url}");var showShareBarUI_params={containerID: "componentDiv${uniq}", shareButtons: [{provider: "twitter", iconImgUp: "http://www.tampabay.com/projects/assets/sharing/twitter_inline.png"}], showCounts: "none", iconsOnly: "true", userAction: act};var params = {userAction:act, enabledProviders: "twitter"};</script><span class="componentDivInline" id="componentDiv${uniq}"></span></span><script type="text/javascript">gigya.socialize.showShareBarUI(showShareBarUI_params);</script>`;
+              });
+              tweetline.forEach((e, i) => {
+                txt = txt.replace(/tweet{.+}/, replacer[i]);
+              });
+              return txt;
+            } else {
+              return txt;
+            }
+          }
+        },
+        {
           name: "slugify",
           fn: function(txt, opts) {
             return slug(txt, opts);
